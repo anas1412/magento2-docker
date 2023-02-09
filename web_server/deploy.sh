683db47964dfa2e221e98bde4fc8b4814d0f5a50
anas1412 <anas.bassoumi@gmail.com>
2023-02-09 15:35:51 +0100

    Updated docker files

--- web_server/deploy.sh
+++ web_server/deploy.sh
@@ -5,8 +5,8 @@ chmod u+x bin/magento
 update-alternatives --set php /usr/bin/php7.2
 composer self-update --1
 composer install --ignore-platform-reqs
-php bin/magento setup:config:set --db-host "mysql_db" --db-user="root" --db-password="$MYSQL_ROOT_PASSWORD" --db-name="$MYSQL_DATABASE"
-#php bin/magento setup:config:set --db-host "mysql_db" --db-user="root" --db-password="magento2" --db-name="kamioun"
+php bin/magento setup:config:set --db-host "mysql_db" --db-user="magento2" --db-password="$MYSQL_ROOT_PASSWORD" --db-name="$MYSQL_DATABASE"
+#php bin/magento setup:config:set --db-host "mysql_db" --db-user="magento2" --db-password="magento2" --db-name="kamioun"
 echo "Magento Installed"
 
 rm -rf var/cache/* var/page_cache/* var/generation/*
@@ -16,7 +16,8 @@ bin/magento cache:clean
 php bin/magento cache:flush
 php bin/magento setup:static-content:deploy -f
 chown -R kamiounusr:kamiounusr .
-#mysql -u "root" -p"Kamioun147#" kamioun_preprod_newtest -e "UPDATE core_config_data SET value = 'http://backoffice-preprod.kamioun.com/' WHERE path = 'web/secure/base_url';"
+#mysql -u "magento2" -p"magento2" kamioun -e "UPDATE core_config_data SET value = 'http://127.0.0.1/backend-magento/' WHERE path = 'web/secure/base_url';"
+#mysql -u "magento2" -p"magento2" kamioun -e "UPDATE core_config_data SET value = 'http://127.0.0.1/backend-magento/' WHERE path = 'web/unsecure/base_url';"
 bin/magento cache:clean
-supervisorctl stop deploy_magento && supervisorctl remove deploy_magento
+#supervisorctl stop deploy_magento && supervisorctl remove deploy_magento
 echo "Magento Deployed"
