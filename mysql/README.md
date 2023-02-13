2a78f4001223dc954a1595a9481920d3c6f1ab64
anas1412 <anas.bassoumi@gmail.com>
2023-02-13 14:51:36 -0800

    Update docker files

--- mysql/README.md
+++
@@ -1,5 +0,0 @@
-### Setting Up Magento 2 MYSQL DB
-
-Execute the following command in the mysql container to import your db dump:
-
-`mysql -u root -p kamioun < kamioun.sql`
--- web_server/deploy.sh
+++ web_server/deploy.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
-
+php bin/magento module:enable --all
 chown -R www-data: /var/www/
 chmod u+x bin/magento
 update-alternatives --set php /usr/bin/php7.2
 composer self-update --1
-composer install --ignore-platform-reqs
-sudo -u www-data bin/magento setup:install --base-url=http://127.0.0.1/backend-magento --db-host=mysql_db --db-name=kamioun --db-user=magento2 --db-password=magento2 --admin-firstname=admin --admin-lastname=admin --admin-email=admin@example.com --admin-user=admin --admin-password=admin123 --language=en_US --currency=USD --timezone=America/Chicago --use-rewrites=1
-
-echo "Magento Installed"
+composer install
+bin/magento setup:install --base-url=http://127.0.0.1/backend-magento --db-host=mysql_db --db-name=kamioun --db-user=magento2 --db-password=magento2 --admin-firstname=admin --admin-lastname=admin --admin-email=admin@example.com --admin-user=admin --admin-password=admin123 --language=en_US --currency=USD --timezone=America/Chicago --use-rewrites=1
 bin/magento cron:install
-#ADD CRON TO WEB SERVER
+
 rm -rf var/cache/* var/page_cache/* var/generation/*
-bin/magento setup:upgrade
-bin/magento setup:di:compile
+php bin/magento setup:upgrade
+php bin/magento setup:di:compile
+php bin/magento setup:static-content:deploy -f
 bin/magento cache:clean
 php bin/magento cache:flush
-php bin/magento setup:static-content:deploy -f
 
-php bin/magento setup:store-config:set --base-url="http://backend-magento.magento.local"
-php bin/magento setup:store-config:set --base-url-secure="https://backend-magento.magento.local"
+find . -type f -exec chmod 644 {} \;            
+find . -type d -exec chmod 755 {} \;        
+chmod -Rf 777 var pub
+chmod 777 ./app/etc
+chmod 644 ./app/etc/*.xml
+chmod -Rf 775 bin
+
+#php bin/magento setup:store-config:set --base-url="http://backend-magento.magento.local"
+#php bin/magento setup:store-config:set --base-url-secure="https://backend-magento.magento.local"
 
 chown -R kamiounusr:kamiounusr .
-#mysql -u "magento2" -p"magento2" kamioun -e "UPDATE core_config_data SET value = 'http://127.0.0.1/backend-magento/' WHERE path = 'web/secure/base_url';"
-#mysql -u "magento2" -p"magento2" kamioun -e "UPDATE core_config_data SET value = 'http://127.0.0.1/backend-magento/' WHERE path = 'web/unsecure/base_url';"
 bin/magento cache:clean
+
 #supervisorctl stop deploy_magento && supervisorctl remove deploy_magento
 echo "Magento Deployed"
