b5e4cb521bceb712f432cc2156604833a8e4c065
anas1412 <anas.bassoumi@gmail.com>
2023-02-16 02:34:58 -0800

    updated menu

--- menu.sh
+++ menu.sh
@@ -28,53 +28,55 @@ while true; do
     read -r choice
     case $choice in
         1)
-            echo "Installing Docker"
-            apt-update && apt install docker.io docker-compose -y
-            echo "Docker installed"
+            echo "//Updating packages//"
+            apt update -y
+            echo "//Installing Docker//"
+            apt install docker.io docker-compose -y
+            echo "//Docker installed//"
             ;;
         2)
-            echo "Run Docker Compose"
+            echo "//Run Docker Compose//"
             docker-compose up -d
-            echo "use docker ps to check if containers are running"
+            echo "//use docker ps to check if containers are running//"
             ;;
         3)
-            echo "Stop Docker Compose"
+            echo "//Stop Docker Compose//"
             docker-compose down
             ;;
         4)
-            echo "Installing Database"
+            echo "//Installing Database//"
             chmod a+x ./scripts/install_db.sh && ./scripts/install_db.sh
             ;;
         5)
-            echo "Installing composer and necessary dependencies"
+            echo "//Installing composer and necessary dependencies//"
             chmod a+x ./scripts/install_composer.sh && ./scripts/install_composer.sh
             ;;
         6)
-            echo "Installing magento store"
+            echo "//Installing magento store//"
             chmod a+x ./scripts/install_magento.sh && ./scripts/install_magento.sh
             ;;
         7)
-            echo "Upgrading and deploying magento store"
+            echo "//Upgrading and deploying magento store//"
             chmod a+x ./scripts/upgrade_magento.sh && ./scripts/upgrade_magento.sh
             ;;
         8)
-            echo "Cleaning cache"
+            echo "//Cleaning cache//"
             chmod a+x ./scripts/clean_cache.sh && ./scripts/clean_cache.sh
             ;;
         9)
-            echo "Setting permissions"
+            echo "//Setting permissions//"
             chmod a+x ./scripts/set_permissions.sh && ./scripts/set_permissions.sh
             ;;
         10)
-            echo "Setting URL"
+            echo "//Setting URL//"
             chmod a+x ./scripts/set_url.sh && ./scripts/set_url.sh
             ;;
         11)
-            echo "Exiting"
+            echo "//Exiting//"
             exit 0
             ;;
         *)
-            echo "Invalid option"
+            echo "//Invalid option//"
             ;;
     esac
 done
